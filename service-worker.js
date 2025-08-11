9740f239c532ef5a03ac5ce6a43770c34c4ae7a4
Dexploarer <dexploarer@gmail.com>
2025-08-11 16:18:50 -0400

    Initial commit: PCTC Practical Nursing Checklist (offline, print-ready, JSON/CSV export, e-signatures)

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pctc-checklist-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/pctc-checklist.html',
+  '/manifest.webmanifest',
+  '/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
+
+
